99820edc0d4f720b8485e2db245a029236f35911
tarmets <dadacable@gmail.com>
2019-10-25 22:59:00 +0300

    Update start.sh

--- add/start.sh
+++ add/start.sh
@@ -1,1 +1,16 @@
 #!/bin/ash
+
+python3.7 /opt/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
+
+# service apache2 start >/dev/null 2>&1 &
+
+# service cron start >/dev/null 2>&1 &
+
+# cd /opt/linux-x64/
+# chmod +x RemoteForkCP
+# ./RemoteForkCP $1 >/dev/null 2>&1 &
+
+while true; do
+        sleep 600
+        rm -rf /tmp/.ACEStream/collected_torrent_files/*
+done
